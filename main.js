e633d64b8a577d2dc8592219588c3a924123ff6c
Pedro Xavier <pedroxxavier@hotmail.com>
2023-08-15 15:53:58 -0300

    Manipulando objetos no DOM

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ let listaDeItens = [];
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
-const ulITens = document.getElementById("lista-de-itens");
+const ulItens = document.getElementById("lista-de-itens");
+const ulItensComprados = document.getElementById("itens-comprados");
 
 form.addEventListener("submit", function (evento) {
   evento.preventDefault();
-  salvarItens();
+  salvarItem();
   mostrarItem();
+  itensInput.focus();
 });
 
-function salvarItens() {
+function salvarItem() {
   const comprasItem = itensInput.value;
   const checarDuplicado = listaDeItens.some(
     (elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase()
@@ -21,16 +23,32 @@ function salvarItens() {
   } else {
     listaDeItens.push({
       valor: comprasItem,
+      checar: false,
     });
   }
 
-  console.log(listaDeItens);
+  itensInput.value = "";
 }
 
 function mostrarItem() {
-  ulITens.innerHTML = "";
+  ulItens.innerHTML = "";
+  ulItensComprados.innerHTML = "";
+
   listaDeItens.forEach((elemento, index) => {
-    ulITens.innerHTML += `
+    if (elemento.checar) {
+      ulItensComprados.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" checked class="is-clickable" />  
+            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `;
+    } else {
+      ulItens.innerHTML += `
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
@@ -41,5 +59,17 @@ function mostrarItem() {
         </div>
     </li>
     `;
+    }
+  });
+
+  const inputsCheck = document.querySelectorAll('input[type="checkbox"]');
+
+  inputsCheck.forEach((i) => {
+    i.addEventListener("click", (evento) => {
+      valorDoElemento =
+        evento.target.parentElement.parentElement.getAttribute("data-value");
+      listaDeItens[valorDoElemento].checar = evento.target.checked;
+      mostrarItem();
+    });
   });
 }
